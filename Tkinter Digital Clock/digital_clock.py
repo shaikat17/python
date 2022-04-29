468cd48379b2bd71089ec79722f8d63be9810209
Shaikat <49788830+shaikat17@users.noreply.github.com>
2022-04-30 00:40:47 +0600

    Add files via upload

---
+++ Tkinter Digital Clock/digital_clock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import *
+
+root = Tk()
+root.title("TKinter Digital Clock")
+
+def time():
+	string = strftime("%I:\n%M:\n%S:\n%p")
+	label.config(text=string)
+	label.after(1000,time)
+	
+label = Label(root,font=("Arial",125),background="green",foreground="white")
+label.pack(anchor = "center")
+
+time()
+
+mainloop()
